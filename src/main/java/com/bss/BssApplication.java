8fd44a1808895d6b927944a04142da2e1b6e9258
taoAtao <xiangtao1994@hotmail.com>
2019-05-09 13:56:30 +0800

    bbs

---
+++ src/main/java/com/bss/BssApplication.java
@@ -0,0 +1,13 @@
+package com.bss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BssApplication.class, args);
+    }
+
+}
